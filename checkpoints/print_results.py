c6e42b8ac48c5df04bae6ef64bb2db0e39f332f2
Jack Turner <jackwilliamturner@icloud.com>
2021-07-27 10:27:34 +0100

    [checkpoints] add a result printer

---
+++ checkpoints/print_results.py
@@ -0,0 +1,23 @@
+import os 
+import torch 
+
+import pandas as pd
+from tabulate import tabulate
+
+checkpoint_files = [f for f in os.listdir('.') if f.endswith(".t7")]
+
+df = []
+
+for checkpoint_file in checkpoint_files:
+    sd = torch.load(checkpoint_file)
+
+    epoch = sd['epoch']
+    errs  = sd['error_history'][-1]
+
+    df.append([checkpoint_file, epoch, errs])
+
+df = pd.DataFrame(df, columns=['Filename', 'Epoch', 'Latest Error'])
+
+print(tabulate(df, headers="keys", tablefmt="psql"))
+
+df.to_pickle("latest_results.pd")
